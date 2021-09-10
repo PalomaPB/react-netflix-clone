7836c2ce6ca49d9b5ed8b1a6e248c687235a7259
PalomaPB <palomabarroqueiro@outlook.com>
2021-09-10 12:09:50 -0300

    requisição ao servidor

--- netflixClone/src/pages/login/index.js
+++ netflixClone/src/pages/login/index.js
@@ -1,56 +1,98 @@
-import React, {useState} from 'react';
-import {View, Image, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Image, Text, ActivityIndicator} from 'react-native';
 import {TextInput, Button} from 'react-native-paper';
+
+import api from '../../services/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import styles from './style';
 
 const Login = ({navigation}) => {
+  //0 - carregando, 1 - logado, 2 - deslogado
+  const [logged, setLogged] = useState(0);
   const [credenciais, setCredenciais] = useState({
     email: '',
     senha: '',
   });
 
+  const checkLogin = async () => {
+    const user = await AsyncStorage.getItem('@user');
+
+    if (user) {
+      setLogged(1);
+      navigation.replace('Home');
+    } else {
+      setLogged(2);
+    }
+  };
+
+  const login = async () => {
+    try {
+      const response = await api.post('/usuario/login', credenciais);
+      const res = response.data;
+
+      if (res.error) {
+        alert(res.message);
+        return false;
+      }
+
+      await AsyncStorage.setItem('@user', JSON.stringify(res.usuario));
+      navigation.replace('Home');
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  useEffect(() => {
+    checkLogin();
+  });
+
   return (
     <View style={styles.bgDark}>
       <Image style={styles.logo} source={require('../../assets/logo.png')} />
-      <View>
-        <TextInput
-          mode="flat"
-          label="Email ou número de telefone"
-          style={styles.marginBotton}
-          value={credenciais.email}
-          onChangeText={text => setCredenciais({...credenciais, email: text})}
-        />
+      {logged === 0 && <ActivityIndicator color="#FFF" size="large" />}
 
-        <TextInput
-          mode="flat"
-          label="Senha"
-          style={styles.marginBotton}
-          secureTextEntry
-          value={credenciais.senha}
-          onChangeText={text => setCredenciais({...credenciais, senha: text})}
-        />
+      {logged === 2 && (
+        <View>
+          <TextInput
+            mode="flat"
+            label="Email ou número de telefone"
+            style={styles.marginBotton}
+            value={credenciais.email}
+            onChangeText={text => setCredenciais({...credenciais, email: text})}
+          />
 
-        <Button
-          style={styles.marginBotton}
-          mode="contained"
-          onPress={() => {
-            navigation.navigate('Home');
-          }}>
-          Entrar
-        </Button>
+          <TextInput
+            mode="flat"
+            label="Senha"
+            style={styles.marginBotton}
+            secureTextEntry
+            value={credenciais.senha}
+            onChangeText={text => setCredenciais({...credenciais, senha: text})}
+          />
 
-        <Button
-          style={styles.marginBotton}
-          onPress={() => console.log('Pressed')}>
-          Recuperar Senha
-        </Button>
+          <Button
+            style={styles.marginBotton}
+            mode="contained"
+            onPress={
+              () => login()
+              //{navigation.navigate('Home');}
+            }>
+            Entrar
+          </Button>
 
-        <Text style={styles.textSmall}>
-          O acesso está protegido pelo Google reCAPTCHA para garantir que você
-          não é um robo. Saiba Mais.
-        </Text>
-      </View>
+          <Button
+            style={styles.marginBotton}
+            onPress={() => console.log('Pressed')}>
+            Recuperar Senha
+          </Button>
+
+          <Text style={styles.textSmall}>
+            O acesso está protegido pelo Google reCAPTCHA para garantir que você
+            não é um robo. Saiba Mais.
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
